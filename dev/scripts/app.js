718e93b4456453c0c076805a16945c06d9658cd0
Jackie Lealess <jlealess@gmail.com>
2018-07-16 15:53:08 -0400

    Adding error handling for axios call

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -209,21 +209,29 @@ class App extends React.Component {
         responseType: "json"
       }).then(res => {
         const product = res.data;
-        const matches = [];
-        for (let i = 0; i < featuredProducts.length; i++) {
-          if (featuredProducts[i].id === productId) {
-            matches.push(productId);
+        featuredProducts.push(product);
+        }).catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
           }
-        }
-        if (matches.length === 0) {
-          featuredProducts.push(product);
-        }
-      })
-
-      this.setState({
-        featuredProducts,
-        productsToDisplay: featuredProducts
-      });
+          console.log(error.config);
+        });
+    });
+    this.setState({
+      featuredProducts,
+      productsToDisplay: featuredProducts
     });
   }
 
